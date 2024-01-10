b0b010d6c088aec4c21ac0d36efaebdbd21c9486
rio <hzy1210085279@gmail.com>
2024-01-10 17:33:59 +0800

    delete useless file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { BbbService } from './bbb/bbb.service';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  @Inject(BbbService)
-  private readonly bbbModule: BbbService;
-
   getHello(): string {
-    return 'Hello World!' + this.bbbModule.findAll();
+    return 'Hello World!';
   }
 }
