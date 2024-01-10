ef4649ccd95c28bba8bbc3f65e3905de62124c46
rio <hzy1210085279@gmail.com>
2024-01-10 22:24:25 +0800

    L_9 AOP架构

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { NextFunction } from 'express';
+import { LoginGuard } from './login.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // 将 public 目录映射到 /static
-  app.useStaticAssets('public', { prefix: '/static' });
+
+  // 全局 Middleware
+  app.use(function (req: Request, res: Response, next: NextFunction) {
+    console.log('before', req.url);
+    next();
+    console.log('after');
+  });
+
+  // 全局 Guard
+  // app.useGlobalGuards(new LoginGuard());
   await app.listen(3000);
 }
 bootstrap();
